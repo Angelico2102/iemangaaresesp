189c3c1c53d27e5b92deede5effd7cf8952c3771
Angelico <angel21cotillas@gmail.com>
2023-11-30 21:41:28 +0100

    Beta Web

---
+++ js/tomo2.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mangaPagesContainer = document.getElementById("manga-pages");
+    const prevPageButton = document.getElementById("prevPage");
+    const nextPageButton = document.getElementById("nextPage");
+    const pageSelector = document.getElementById("pageSelector");
+
+    const totalPages = 170; // Cambia esto según tus necesidades
+    let currentPage = 1;
+
+    function loadPage(page) {
+        mangaPagesContainer.innerHTML = `<img id="manga-page" src="../images/tomo2/${page}.png">`;
+    }
+
+    function updatePageSelector() {
+        pageSelector.innerHTML = "";
+        for (let i = 1; i <= totalPages; i++) {
+            const option = document.createElement("option");
+            option.value = i;
+            option.text = `Página ${i}`;
+            pageSelector.appendChild(option);
+        }
+        pageSelector.value = currentPage;
+    }
+
+    prevPageButton.addEventListener("click", function () {
+        if (currentPage > 1) {
+            currentPage--;
+            loadPage(currentPage);
+            updatePageSelector();
+        }
+    });
+
+    nextPageButton.addEventListener("click", function () {
+        if (currentPage < totalPages) {
+            currentPage++;
+            loadPage(currentPage);
+            updatePageSelector();
+        }
+    });
+
+    pageSelector.addEventListener("change", function () {
+        currentPage = parseInt(pageSelector.value);
+        loadPage(currentPage);
+    });
+
+    // Inicializar la página
+    loadPage(currentPage);
+    updatePageSelector();
+});
